96ee217c2b7b635f9c760a5090df2df5c66b08f5
Navid Rojiani <nrojiani@gmail.com>
2017-08-17 22:10:56 -0700

    clean up largest-series-product

--- largest-series-product/src/main/kotlin/Series.kt
+++ largest-series-product/src/main/kotlin/Series.kt
@@ -1,21 +1,10 @@
-/**
- * Series.kt
- *
- * http://exercism.io/exercises/kotlin/largest-series-product/readme
- * http://exercism.io/tracks/kotlin/exercises/largest-series-product
- * http://exercism.io/submissions/87cf9a2133c744b0bb4976172d2730be
- *
- * @author nrojiani
- * @date 8/17/17
- */
 data class Series(private val input: String) {
     init {
         require(input.toList().all { it.isDigit() }) { "Input contains non-digit" }
     }
 
-    /* Convert String to List<Int> */
-    private val series: List<Int> by lazy(mode = LazyThreadSafetyMode.NONE) {
-        input.toList().map { it.toString().toInt() }
+    private val series: List<Long> by lazy(mode = LazyThreadSafetyMode.NONE) {
+        input.toList().map { it.toString().toLong() }
     }
 
     fun getLargestProduct(span: Int): Long {
@@ -23,15 +12,13 @@ data class Series(private val input: String) {
         require(span <= input.length) { "Span must be <= the size of the input" }
 
         return (0..(series.size - span))
-                .foldIndexed(0L) { index, maxProduct, _ ->
+                .foldIndexed(0L) { index, maxSpanProduct, _ ->
 
-                    val spanProduct = series.slice(index until (index + span))
-                            .fold(1.0) { spanProduct, spanElement ->
-                                spanProduct * spanElement.toDouble()
-                            }.toLong()
+                    val spanProduct = series
+                            .slice(index until (index + span))
+                            .fold(1L) { product, n -> product * n }
 
-                    if (spanProduct > maxProduct) spanProduct else maxProduct
-
+                    maxOf(spanProduct, maxSpanProduct)
                 }
     }
 }
