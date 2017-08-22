68d4b035997d22a3553c9a0dec7e18924b7f25ce
Navid Rojiani <nrojiani@gmail.com>
2017-08-21 21:50:00 -0700

    diamond

---
+++ diamond/src/main/kotlin/DiamondPrinter.kt
@@ -0,0 +1,41 @@
+/**
+ * DiamondPrinter
+ *
+ * http://exercism.io/exercises/kotlin/diamond/readme
+ * http://exercism.io/tracks/kotlin/exercises/diamond
+ */
+class DiamondPrinter {
+
+    private val ALPHABET = ('A'..'Z')
+
+    fun printToList(letter: Char): List<String> {
+        if (letter == 'A')
+            return listOf("A")
+
+        val letterIdx = ALPHABET.indexOf(letter)
+        val rowWidth = (letterIdx * 2) + 1
+
+        val above: List<String> = ('A' until letter)
+                .foldIndexed(emptyList()) { index, acc, c ->
+
+                    val endSpacesWidth = letterIdx - index
+                    val endSpaces = spaces(endSpacesWidth)
+
+                    if (index == 0) {
+                        acc + (endSpaces + c + endSpaces)
+                    } else {
+                        val middleSpacesWidth = rowWidth - (endSpacesWidth * 2) - 2
+                        val middleSpaces = spaces(middleSpacesWidth)
+                        acc + (endSpaces + c + middleSpaces + c + endSpaces)
+                    }
+
+                }
+
+        val below = above.reversed()
+        val middle = letter + spaces(rowWidth - 2) + letter
+
+        return above + middle + below
+    }
+
+    private fun spaces(n: Int): String = " ".repeat(n)
+}
