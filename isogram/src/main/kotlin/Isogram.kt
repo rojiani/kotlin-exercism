8a16937368dff3393c52ca61be6c1267633cba83
Navid Rojiani <nrojiani@gmail.com>
2017-08-21 22:30:55 -0700

    clean up isogram

--- isogram/src/main/kotlin/Isogram.kt
+++ isogram/src/main/kotlin/Isogram.kt
@@ -5,18 +5,16 @@ package Isogram
  *
  * http://exercism.io/exercises/kotlin/isogram/readme
  * http://exercism.io/tracks/kotlin/exercises/isogram
- * http://exercism.io/submissions/15659c028b454fb2b31e67ae47fba7f3
+ * http://exercism.io/tracks/kotlin/exercises/isogram
  *
  * @author nrojiani
  * @date 8/21/17
  */
-
-fun isIsogram(input: String): Boolean {
-    return letterFrequencies(input).values.none { it > 1 }
-}
+fun isIsogram(input: String): Boolean = letterFrequencies(input).values.none { it > 1 }
 
 fun letterFrequencies(input: String): Map<Char, Int> =
         input.filter { it.isLetter() }
-                .map { it.toLowerCase() }
+                .toLowerCase()
                 .groupingBy { it }
                 .eachCount()
+
