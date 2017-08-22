6892dfb8ba6bfc5a132d9554f3c74be233180b17
Navid Rojiani <nrojiani@gmail.com>
2017-08-21 22:21:24 -0700

    isogram - use groupingBy() and eachCount()

--- isogram/src/main/kotlin/Isogram.kt
+++ isogram/src/main/kotlin/Isogram.kt
@@ -5,7 +5,7 @@ package Isogram
  *
  * http://exercism.io/exercises/kotlin/isogram/readme
  * http://exercism.io/tracks/kotlin/exercises/isogram
- * http://exercism.io/submissions/4cf7aff9f46f4fd5b3b01d0a53b23981
+ * http://exercism.io/submissions/15659c028b454fb2b31e67ae47fba7f3
  *
  * @author nrojiani
  * @date 8/21/17
@@ -15,13 +15,8 @@ fun isIsogram(input: String): Boolean {
     return letterFrequencies(input).values.none { it > 1 }
 }
 
-
-fun letterFrequencies(input: String): Map<Char, Int> {
-    val map: MutableMap<Char, Int> = mutableMapOf()
-    input.filter { it.isLetter() }
-            .map { it.toLowerCase() }
-            .forEach { ch ->
-                    map.put(ch, map.getOrDefault(ch, 0) + 1)
-            }
-    return map
-}
+fun letterFrequencies(input: String): Map<Char, Int> =
+        input.filter { it.isLetter() }
+                .map { it.toLowerCase() }
+                .groupingBy { it }
+                .eachCount()
