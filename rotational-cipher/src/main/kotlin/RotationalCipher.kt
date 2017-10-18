3cd14692964da3511dea0c84733640fbde0b3e13
Navid Rojiani <nrojiani@gmail.com>
2017-10-17 23:23:32 -0700

    update rotational-cipher

--- rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -3,31 +3,28 @@
  *
  * http://exercism.io/exercises/kotlin/rotational-cipher/readme
  * http://exercism.io/tracks/kotlin/exercises/rotational-cipher
- * http://exercism.io/submissions/c999b8cfcb214d889f2ec2b420b3d435
+ * http://exercism.io/submissions/9c20b61deec94b76bca5475089ee36fb
  *
  * @author nrojiani
  * @date 8/30/17
  */
 data class RotationalCipher(private val shift: Int) {
 
-    private val alphabet = ('a'..'z')
+    fun encode(input: String): String =
+            input.fold("") { acc, ch ->
+                acc + transpose(ch)
+            }
 
-    fun encode(input: String): String = input.fold("") { acc, ch ->
-        acc + when {
-            ch.isLetter() -> transpose(ch)
-            else -> ch
-        }
-    }
+    private fun transpose(char: Char): Char {
 
-    private fun transpose(letter: Char): Char {
-        var transposeLowerCase: (Char) -> Char = {
-            alphabet.elementAt((alphabet.indexOf(it) + shift) % alphabet.count())
+        var transposeChar: (Char, CharRange) -> Char = { c: Char, alphabet: CharRange ->
+            alphabet.elementAt((alphabet.indexOf(c) + shift) % alphabet.count())
         }
 
         return when {
-            letter.isLowerCase() -> transposeLowerCase(letter)
-            letter.isUpperCase() -> transposeLowerCase(letter.toLowerCase()).toUpperCase()
-            else -> throw IllegalArgumentException("Not a letter")
+            char.isLowerCase() -> transposeChar(char, ('a'..'z'))
+            char.isUpperCase() -> transposeChar(char, ('A'..'Z'))
+            else -> char
         }
     }
 }
